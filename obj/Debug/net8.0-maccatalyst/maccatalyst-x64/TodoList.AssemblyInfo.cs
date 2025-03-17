8878ebb4fcec45f8be846b9c02121cb737236c43
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2025-03-17 23:45:27 +0200

    Committing todo list ui updates

--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/TodoList.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+265111930ec45b63075b940ed5eab38f0ec08a62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b1a8738081fa987fbcf9814632beeaf31558cbfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
